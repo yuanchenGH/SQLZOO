238f8d67faf4b221ec3d64e68137ba6c2e8fab0b
yuanchenGH <71296283+yuanchenGH@users.noreply.github.com>
2023-06-02 16:51:13 -0500

    Update More_JOIN.sql

--- More_JOIN.sql
+++ More_JOIN.sql
@@ -17,4 +17,31 @@ WHERE casting.movieid IN (SELECT movieid
                           JOIN movie ON casting.movieid = movie.id
                           JOIN actor ON casting.actorid = actor.id
                           WHERE actor.name = 'Julie Andrews')
+                          
+-- #13
+SELECT actor.name
+FROM actor
+JOIN casting ON actor.id = casting.actorid
+WHERE ord = 1
+GROUP BY actor.name
+HAVING COUNT(*) >= 15
+ORDER BY actor.name
+
+-- #14
+SELECT movie.title, COUNT(*)
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+WHERE yr = 1978
+GROUP by movie.title
+ORDER BY COUNT(casting.actorid) DESC, movie.title
+
+-- #14
+SELECT actor.name
+FROM actor
+JOIN casting ON actor.id = casting.actorid
+WHERE casting.movieid IN (SELECT casting.movieid
+                          FROM actor
+                          JOIN casting ON actor.id = casting.actorid
+                          WHERE actor.name = 'Art Garfunkel')
+      AND actor.name <> 'Art Garfunkel'
 
